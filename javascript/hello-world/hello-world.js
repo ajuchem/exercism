2bca6a800bfb5b99981ccc48ca5e5eb9e40e1fac
ajuchem <ajuchem@daudt.com>
2016-04-07 20:03:16 -0600

    Solution to hello-world exercism

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,8 +1,7 @@
 //
 // This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
+// Make sure to look at test.script.js--that should give you some hints about what is expected here.
 
 var HelloWorld = function() {};
 
